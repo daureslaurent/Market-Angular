4f1e780c5be365a2061b935aa0b09d6f5ea7c5b1
ldaures <daures.laurent@gmail.com>
2026-01-20 11:03:27 +0100

    Keycloak WIP

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,21 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideKeycloak} from 'keycloak-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideKeycloak({
+      config: {
+        url: '',
+        realm: '',
+        clientId: '',
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html'
+      }
+    }),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes)
   ]
